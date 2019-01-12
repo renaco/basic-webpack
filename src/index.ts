1b847f9bc5f43db2c6a8d0802132b0adcf507480
renaco <renanromero@gmail.com>
2019-01-11 21:12:10 -0500

    feat app: add video bg

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
-import * as Index from './modules/index';
-import { DomClass } from './modules/dom';
-let myTitle: string = 'this is my title';
 let myButton = document.querySelectorAll('#myButton')[0];
 let myBody = document.body;
+let myVideo = document.querySelectorAll('iframe')[0];
 
 myButton.addEventListener('click', (e) => {
   myBody.classList ? myBody.classList.add('app-body') : myBody.className += ' ' + 'app-body';
   myButton.classList ? myButton.classList.add('app-body-link') : myButton.className += ' ' + 'app-body-link';
-  console.log('DomClass', typeof DomClass);
-  DomClass();
+  myVideo.style.display = 'block';
 });
 
 
